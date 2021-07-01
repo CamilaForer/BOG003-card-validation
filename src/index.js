c6656c8e0ecb7a94a14c72622e20bd5d10e96754
Camila <amandaforero@outlook.com>
2021-07-01 11:06:48 -0500

    Funcion para ocultar y mostrar pagina de validación

--- src/index.js
+++ src/index.js
@@ -2,3 +2,19 @@ import validator from './validator.js';
 
 console.log(validator);
 
+// Cambio de pagina
+const btn= document.querySelector("#botonCarrito")
+const carrito=document.querySelector("#validationPage")
+const principal=document.querySelector("#principalPage")
+btn.addEventListener("click", cambioPagina)
+
+function cambioPagina(){
+    carrito.classList.remove("hide") 
+    principal.classList.add("hide")
+}
+
+// Llenado tarjeta
+
+
+
+
