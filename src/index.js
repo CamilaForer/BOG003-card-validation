927690dadfb20d56a173cee00b0f483e2d6ed78e
Camila <amandaforero@outlook.com>
2021-07-06 19:53:57 -0500

    funciones para reflejar la select en la tarjeta y eliminar letras del inputCVV

--- src/index.js
+++ src/index.js
@@ -72,13 +72,13 @@ function inputNam(a){
 const expMonth= document.querySelector(".mes")
 const expYear= document.querySelector(".year")
 // Select mes
-selectMes.addEventListener("change", selectMonth)
+expMonth.selectMes.addEventListener("change", selectMonth)
 function selectMonth(b){
     expMonth.textContent=b.target.value;
 }
 
 // Select Año
-selectYear.addEventListener("change", selectYea)
+expYear.selectYear.addEventListener("change", selectYea)
 function selectYea(c){
     expYear.textContent=c.target.value;
 }
@@ -89,7 +89,6 @@ inputCVV.addEventListener("keyup", inputCv)
 function inputCv(c){
     let cvvValue=c.target.value;
     inputCVV.value=cvvValue.replace(/([A-Za-z])/g,"")
-
 }
 
 
